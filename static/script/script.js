f03d56dd9fd48d8b5b662542bec35c7e801c9971
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-09-22 12:59:18 -0700

    Adds more comments for better understandability

--- static/script/script.js
+++ static/script/script.js
@@ -1,15 +1,27 @@
+// Getting the theme (dark mode or light mode) from local storage
 var mode = window.localStorage.getItem("mode") 
+
+// if the theme is currently stored (meaning its not null) and if mode is dark mode then change to dark mode
+// Otherwise by default its light mode
 if(mode != null){
   if(mode == "dark"){
     changeModeToDark();
     document.getElementById("checkbox").checked=true;
   }
 }
+
+// Get language from local storage and set our pages language to that value
+// by default its english 
 var language = window.localStorage.getItem("language") 
 if(language != null){
   load_from_json(language)
 }else{
-load_from_json("english")}
+  load_from_json("english")
+}
+
+// Function to load from json file
+// the json file is picked based of the language chosen by the user
+// and then all the data is injected into the DOM
 function load_from_json(language){
   window.localStorage.setItem("language",language)
   document.getElementById("lang_dd").textContent = language
@@ -20,8 +32,8 @@ fetch(`/static/languages/${language}.json`)
   )
   .then((data) => {
 responseData=data; // Now data is available 
-//Insert data into DOM 
 
+//Inject data into DOM 
 document.getElementsByClassName("about")[0].textContent = responseData.about;
 document.getElementsByClassName("project")[0].textContent = responseData.projectstxt;
 document.getElementById("project").textContent = responseData.projectstxt;
@@ -80,6 +92,8 @@ responseData.skills.forEach(element=>{
 }) 
 document.getElementById("skill_inject").innerHTML=injection
 }) }
+
+// function to switch mode when our checkbox (switch) is clicked
 function switchMode(e){
   if(e.checked){
     window.localStorage.setItem("mode","dark")
@@ -89,6 +103,8 @@ function switchMode(e){
     changeModeToLight()  
   }
 }
+
+//  these functions (changeModeToDark & changeModeToLight) changes the src of our link tags to css files of corresponding theme 
 function changeModeToDark(){
   var maincss = document.getElementById("maincss")
   var leftcss = document.getElementById("leftcss")
